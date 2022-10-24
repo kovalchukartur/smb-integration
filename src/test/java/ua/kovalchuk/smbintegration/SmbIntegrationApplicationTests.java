db1e3aecac862f1ec9b7b96c3db20b89649d853d
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-24 17:01:31 +0300

    Init

---
+++ src/test/java/ua/kovalchuk/smbintegration/SmbIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.smbintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
